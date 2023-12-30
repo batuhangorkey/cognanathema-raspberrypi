14d672e33a25aaa19a29ff61d7ce3750758d0752
batuhan <gorkeybatuhan@gmail.com>
2023-12-30 03:40:39 +0000

    update

--- client.py
+++ client.py
@@ -1,16 +1,17 @@
 import csv
-from datetime import datetime
-from functools import wraps
 import io
+import json
 import logging
 import multiprocessing
-from multiprocessing import Process
-from queue import Queue
 import queue
 import signal
 import sys
 import threading
 import time
+from datetime import datetime
+from functools import wraps
+from multiprocessing import Process
+from queue import Queue
 from typing import Dict, List
 
 import adafruit_mlx90640
@@ -23,11 +24,11 @@ import numpy as np
 import requests
 import scipy
 import socketio
+from deepface import DeepFace
+from numpy import ndarray
 from picamera2 import Picamera2, Preview
 from PIL import Image
 from scipy import ndimage
-from deepface import DeepFace
-
 
 logger = logging.getLogger("cogna")
 logger.setLevel(logging.DEBUG)
@@ -48,7 +49,10 @@ TARGET_SHAPE = (480, 640)
 CAMERA_SIZE_FULL = (1640, 1232)
 CAMERA_SIZE_LOW = (640, 480)
 
-SERVER_URL = "https://d96f-94-55-176-14.ngrok-free.app"
+SCALING_FACTOR_X = CAMERA_SIZE_FULL[0] / TARGET_SHAPE[1]
+SCALING_FACTOR_Y = CAMERA_SIZE_FULL[1] / TARGET_SHAPE[0]
+
+SERVER_URL = "https://5cc9-94-55-176-14.ngrok-free.app"
 
 CM = mpl.colormaps["bwr_r"]  # type: ignore
 DEAD_PIXEL = np.unravel_index(212, SENSOR_SHAPE)
@@ -57,7 +61,7 @@ KERNEL = np.array([[1, 1, 1], [1, 0, 1], [1, 1, 1]], dtype=bool)
 REQUEST_TIMEOUT = 20
 
 # logger güzel ama çok kalabalık yapıyor
-SIO = socketio.Client(logger=False, reconnection_attempts=5)
+SIO = socketio.Client(logger=False)
 
 # 64 bit Raspbian os does not support picamerav1
 # ALLAH KITAP ASKINDA NEFRET ETTIM, böyle bir api yazılmaz
@@ -74,6 +78,20 @@ SIO = socketio.Client(logger=False, reconnection_attempts=5)
 csv_file_path = "function_times.csv"
 
 
+def log_to_csv(funcname, total_time):
+    with open(csv_file_path, "a", newline="") as csvfile:
+        fieldnames = ["Function", "Execution Time"]
+        writer = csv.DictWriter(csvfile, fieldnames=fieldnames)
+        if csvfile.tell() == 0:
+            writer.writeheader()
+        writer.writerow(
+            {
+                "Function": funcname,
+                "Execution Time": "%.2f" % total_time,
+            }
+        )
+
+
 def timeit(func):
     @wraps(func)
     def timeit_wrapper(*args, **kwargs):
@@ -85,21 +103,7 @@ def timeit(func):
         total_time = end_time - start_time
 
         # first item in the args, ie `args[0]` is `self`
-        logger.info(f"Function {func.__name__} Took {total_time:.2f} seconds")
-
-        with open(csv_file_path, "a", newline="") as csvfile:
-            fieldnames = ["Function", "Execution Time"]
-            writer = csv.DictWriter(csvfile, fieldnames=fieldnames)
-            # If the file is empty, write the header
-            if csvfile.tell() == 0:
-                writer.writeheader()
-            # Write function name and execution time to the CSV file
-            writer.writerow(
-                {
-                    "Function": func.__name__,
-                    "Execution Time": "%.2f" % total_time,
-                }
-            )
+        # logger.info(f"Function {func.__name__} Took {total_time:.2f} seconds")
 
         return result
 
@@ -107,9 +111,6 @@ def timeit(func):
 
 
 def cut_area(area_to_cut, frame) -> np.ndarray:
-    SCALING_FACTOR_X = CAMERA_SIZE_FULL[0] / TARGET_SHAPE[1]
-    SCALING_FACTOR_Y = CAMERA_SIZE_FULL[1] / TARGET_SHAPE[0]
-
     scaled_x = int(area_to_cut["x"] / SCALING_FACTOR_X)
     scaled_y = int(area_to_cut["y"] / SCALING_FACTOR_Y)
     scaled_w = int(area_to_cut["w"] / SCALING_FACTOR_X)
@@ -122,11 +123,43 @@ def cut_area(area_to_cut, frame) -> np.ndarray:
         new_area = frame[
             scaled_y : scaled_y + scaled_h, scaled_x : scaled_x + scaled_w
         ]
-
     return new_area
 
 
-def post_face(image: Image.Image | np.ndarray, thermal_image, thermal_data):
+def send_cam_frame(frame, cam_buffer):
+    image = Image.fromarray(frame)
+    image.save(cam_buffer, "jpeg")
+    cam_buffer.seek(0)
+    data = {
+        "type": "cam",
+        "frame": cam_buffer.getvalue(),
+    }
+    cam_buffer.seek(0)
+    cam_buffer.truncate(0)
+    SIO.emit("live_stream", data)
+
+
+def send_thermal_image(data, frame, buffer):
+    image = Image.fromarray(frame)
+    image.save(buffer, "jpeg")
+    min_value = data.min()
+    max_value = data.max()
+    mean_value = data.mean()
+    data = {
+        "type": "thermal",
+        "frame": buffer.getvalue(),
+        "min": "%.2f" % min_value,
+        "max": "%.2f" % max_value,
+        "mean": "%.2f" % mean_value,
+    }
+    buffer.seek(0)
+    buffer.truncate(0)
+    SIO.emit("live_stream", data)
+
+
+def post_face(
+    image: Image.Image | ndarray, thermal_image: ndarray, temps: dict
+):
     # image_bytes = io.BytesIO()
     if isinstance(image, Image.Image):
         arr = np.asarray(image)
@@ -143,13 +176,17 @@ def post_face(image: Image.Image | np.ndarray, thermal_image, thermal_data):
 
     timestamp = datetime.utcnow().ctime()
 
-    data = {"timestamp": timestamp, "mean_temp": thermal_data.mean()}
+    data = {"timestamp": timestamp}
+    data.update(temps)
 
     try:
         url = SERVER_URL + "/api/recognize"
         logger.info("POSTING TO %s" % url)
         response = requests.post(
-            url, files=files, data=data, timeout=REQUEST_TIMEOUT  # type: ignore
+            url,
+            files=files,
+            data=data,
+            timeout=REQUEST_TIMEOUT,
         )
     except Exception as e:
         logger.info(e)
@@ -158,6 +195,8 @@ def post_face(image: Image.Image | np.ndarray, thermal_image, thermal_data):
         if response.status_code == 200:
             res = response.json()
             logger.info(res)
+        else:
+            logger.info("Response: %s" % response.text)
     finally:
         pass
 
@@ -169,7 +208,10 @@ def process_frame(
     # cam frame BGR formatında
     try:
         faces = DeepFace.extract_faces(
-            cam_frame, enforce_detection=True, align=False
+            cam_frame,
+            enforce_detection=True,
+            align=True,
+            detector_backend="ssd",
         )
         counter.value = counter.value + 1  # type: ignore
     except ValueError:
@@ -185,29 +227,33 @@ def process_frame(
         f"Face Counter: {counter.value}"  # type: ignore
     )
 
-    if counter.value < 0:  # type: ignore
+    if counter.value < 5:  # type: ignore
         return
 
     for face in faces:
         logger.info(f"Face confidence: {face['confidence']}")
         logger.info(f"Face area: {face['facial_area']}")
 
-        if face["confidence"] < 7:
+        if face["confidence"] < 0.96 or face["facial_area"]["w"] < 300:
             continue
 
         face_array = (face["face"] * 255).astype(np.uint8)
 
-        face_thermal_arr = cut_area(face["facial_area"], thermal_frame)
+        face_thermal_rgb = cut_area(face["facial_area"], thermal_frame)
         face_thermal_data = cut_area(face["facial_area"], thermal_data)
+        temps = {
+            "face_mean": face_thermal_data.mean(),
+            "scene_max": thermal_data.max(),
+            "scene_min": thermal_data.min(),
+            "scene_mean": thermal_data.mean(),
+        }
 
-        logger.info(face_thermal_data.mean())
-
-        cv.imwrite("thermal_cut.jpg", face_thermal_arr)
+        cv.imwrite("thermal_cut.jpg", face_thermal_rgb)
         cv.imwrite("thermal_whole.jpg", thermal_frame)
         cv.imwrite("face_cut.jpg", face_array)
-        cv.imwrite("face_whole.jpg", cam_frame)
+        cv.imwrite("face_whole.jpg", cam_frame[:, :, ::-1])
 
-        post_face(face_array, face_thermal_arr, face_thermal_data)
+        post_face(face_array, face_thermal_rgb, temps)
 
         counter.value = 0  # type: ignore
 
@@ -215,7 +261,10 @@ def process_frame(
 @timeit
 def get_thermal_frame(thermal_buffer):
     try:
+        start = time.monotonic()
         MLX.getFrame(thermal_buffer)  # type: ignore
+        logger.info(f"Read time of thermal camera: {time.monotonic() - start}")
+
     except ValueError:
         return None
     frame = thermal_buffer.reshape(SENSOR_SHAPE)
@@ -229,6 +278,7 @@ def get_thermal_frame(thermal_buffer):
     )
     frame[DEAD_PIXEL] = a
     frame = np.fliplr(frame)
+
     frame_upscaled = ndimage.zoom(frame, 20)  # spline interpolation
     # min-max feature scaling metodu. data range'i [0, 1] e getirir
     normalized: np.ndarray = (frame_upscaled - frame.min()) / (
@@ -245,48 +295,47 @@ class ThermalProcess(Process):
 
         self.stop_flag = stop_flag
         self.streaming_flag = streaming_flag
-        self.queue = queue
+        self.queue: multiprocessing.Queue = queue
+        self.flag = multiprocessing.Event()
 
     def run(self):
         logger.info("Thermal loop started")
         thermal_buffer: np.ndarray = np.zeros(24 * 32)
-        last = time.monotonic()
 
         while not self.stop_flag.is_set():
+            start = time.monotonic()
+            if not self.queue.empty():
+                continue
             ret = get_thermal_frame(thermal_buffer)
             if ret is None:
                 continue
             try:
                 frame, data = ret
                 self.queue.put((frame, data), block=False)
-
+                # self.flag.set()
             except queue.Full:
                 pass
             except Exception as e:
                 logger.info(e)
 
-            now = time.monotonic()
-            logger.info(f"Time taken at thermal loop: {now - last}")
-            last = now
-
+            end = time.monotonic()
+            logger.info(f"Time taken at thermal loop: {end - start}")
         logger.info("Thermal loop finished")
 
 
 class CameraProcess(Process):
-    def __init__(self, stop_flag, queue, streaming_flag):
+    def __init__(self, stop_flag, queue):
         Process.__init__(self)
-        self.counter = multiprocessing.Value("i", 0)
 
         self.stop_flag = stop_flag
-        self.streaming_flag = streaming_flag
         self.queue = queue
+        self.streaming_flag = multiprocessing.Event()
 
     def run(self):
         # picam multiprocess de sıkıntı çıkarıyor
         # onun yerine doğrudan fonksiyonun içine aldım
         logger.info("Cam loop started")
-        cam_buffer = io.BytesIO()
-
+        # cam_buffer = io.BytesIO()
         picam2 = Picamera2()
         picam2.start_preview(Preview.NULL)  # type: ignore
         video_config = picam2.create_video_configuration(
@@ -297,6 +346,7 @@ class CameraProcess(Process):
         picam2.start()  # type: ignore
 
         while not self.stop_flag.is_set():
+            start = time.monotonic()
             frame: np.ndarray = picam2.capture_array()  # type: ignore
             # 4. katman ne bilmiyorum
             frame = frame[:, :, :3]
@@ -304,20 +354,11 @@ class CameraProcess(Process):
                 self.queue.put(frame, block=False)
             except queue.Full:
                 pass
-
-            if self.streaming_flag.is_set():
-                image = Image.fromarray(frame)
-                image.save(cam_buffer, "jpeg")
-                cam_buffer.seek(0)
-                data = {
-                    "type": "cam",
-                    "frame": cam_buffer.getvalue(),
-                }
-                cam_buffer.seek(0)
-                cam_buffer.truncate(0)
-
-                SIO.emit("live_stream", data)
-            time.sleep(0.3)
+            # if self.streaming_flag.is_set():
+            #    send_frame(frame, cam_buffer)
+            time.sleep(0.5)
+            now = time.monotonic()
+            logger.info(f"Time at Camera Process: {now - start}")
 
         picam2.close()
         logger.info("Cam loop finished")
@@ -326,86 +367,60 @@ class CameraProcess(Process):
 class Organizer:
     def __init__(self):
         self.clients = {}
-        self.tasks: Dict[str, multiprocessing.Process] = {}
+
+        self.lock = threading.Lock()
+
+        self.tasks = {}
         self.counter = multiprocessing.Value("i", 0)
 
         self.start_flag = threading.Event()
         self.stop_stream_flag = threading.Event()
         self.stop_flag = multiprocessing.Event()
+        self.process_done = multiprocessing.Event()
 
         self.cam_queue = multiprocessing.Queue(maxsize=1)
         self.thermal_queue = multiprocessing.Queue(maxsize=1)
-        self.thermal_data_queue = multiprocessing.Queue(maxsize=1)
 
         self.start()
 
     def start(self):
-        self.tasks["main_loop"] = multiprocessing.Process(
+        self.tasks["main_loop"] = threading.Thread(
             target=self.main_loop, name="Main Loop"
         )
         self.tasks["thermal_loop"] = ThermalProcess(
             self.stop_flag, self.thermal_queue, self.start_flag
         )
-        self.tasks["cam_loop"] = CameraProcess(
-            self.stop_flag, self.cam_queue, self.start_flag
-        )
+        self.tasks["cam_loop"] = CameraProcess(self.stop_flag, self.cam_queue)
 
         for task in self.tasks.values():
             task.start()
 
-    def thermal_loop(self):
-        logger.info("Thermal loop started")
-        thermal_buffer: np.ndarray = np.zeros(24 * 32)
-        last = time.monotonic()
-
-        while not self.stop_flag.is_set():
-            ret = get_thermal_frame(thermal_buffer)
-            if ret is None:
-                continue
-            try:
-                frame, data = ret
-                self.thermal_data_queue.put(data, block=False)
-                self.thermal_queue.put(frame, block=False)
-            except queue.Full:
-                pass
-            except Exception as e:
-                logger.info(e)
-
-            now = time.monotonic()
-            logger.info(f"Time taken at thermal loop: {now - last}")
-            last = now
-
-        logger.info("Thermal loop finished")
-
-    def send_thermal_image(self):
-        min_value = self.thermal_buffer.min()
-        max_value = self.thermal_buffer.max()
-        mean_value = self.thermal_buffer.mean()
-
-        data = {
-            "type": "thermal",
-            "frame": self.thermal_stream.getvalue(),
-            "min": "%.2f" % min_value,
-            "max": "%.2f" % max_value,
-            "mean": "%.2f" % mean_value,
-        }
-        self.thermal_stream.seek(0)
-        self.thermal_stream.truncate(0)
-
-        SIO.emit("live_stream", data)
-
     def main_loop(self):
         logger.info("Main loop started...")
         last = time.monotonic()
+        # last_cam_frame = time.monotonic()
+        cam_buffer = io.BytesIO()
+        thermal_buffer = io.BytesIO()
 
         while not self.stop_flag.is_set():
             frame = self.cam_queue.get()
             thermal_frame, thermal_data = self.thermal_queue.get()
-
+            # now - last_cam_frame
+            # last_cam_frame = now
+            # state = 0
+            # logger.info("All frames gathered. Ready to proceed")
             process_frame(self.counter, frame, thermal_frame, thermal_data)
 
+            if self.start_flag.is_set():
+                try:
+                    send_cam_frame(frame, cam_buffer)
+                    send_thermal_image(
+                        thermal_data, thermal_frame[:, :, ::-1], thermal_buffer
+                    )
+                except Exception:
+                    pass
             now = time.monotonic()
-            logger.info(f"Time taken at main process: {now - last}")
+            logger.debug(f"Time taken at main process: {now - last}")
             last = now
 
     def request_stream(self, client):
@@ -421,15 +436,8 @@ class Organizer:
             if not self.start_flag.is_set():
                 SIO.send(data)
                 logger.info("Starting new threads")
-
-                # feeds = [self.thermal_feed, self.camera_feed]
-
-                # for feed in feeds:
-                #     task = threading.Thread(target=feed)
-                #     self.tasks.append(task)
-                #     task.start()
-
                 self.start_flag.set()
+                self.tasks["cam_loop"].streaming_flag.set()
             else:
                 # we are already streaming
                 SIO.send(data)
@@ -450,27 +458,20 @@ class Organizer:
             logger.info("Client %s does not exists" % client)
             self.clients.clear()
             logger.info("Releasing the lock...")
-            self.lock.release()
-
             self.stop_streaming()
-
+            self.lock.release()
             return False
 
         logger.info("Releasing the lock...")
-        self.lock.release()
 
         if len(self.clients) <= 0:
             self.stop_streaming()
 
+        self.lock.release()
+
     def stop_streaming(self):
         with self.lock:
             self.stop_stream_flag.set()
-            # feeds = ["thermal_feed", "camera_feed"]
-
-            # for task in self.tasks:
-            #     if task.name in feeds:
-            #         task.join()
-
             self.start_flag.clear()
 
     def cleanup(self):
@@ -483,11 +484,6 @@ class Organizer:
 
         try:
             self.cam_queue.get_nowait()
-        except queue.Empty:
-            pass
-
-        try:
-            self.thermal_data_queue.get_nowait()
         except queue.Empty:
             pass
 
@@ -546,5 +542,5 @@ if __name__ == "__main__":
     signal.signal(signal.SIGTERM, exit_handler)
     signal.signal(signal.SIGUSR2, exit_handler)
 
-    SIO.connect(SERVER_URL, wait_timeout=5)
+    SIO.connect(SERVER_URL, wait_timeout=50)
     SIO.wait()
--- temp.sh
+++ temp.sh
@@ -4,10 +4,11 @@ while true
 do
     cpu=$(</sys/class/thermal/thermal_zone0/temp)
     cpu_s=$(vcgencmd measure_temp | awk -F "[=']" '{print($2)}')
+    cpu=$(bc <<< "scale=2; $cpu/1000")
 
     echo "$(date) @ $(hostname)"
     echo "-------------------------------------------"
-    echo "CPU (thermal zone) => $((cpu/1000))'C"
+    echo "CPU (thermal zone) => ${cpu}'C"
     echo "CPU (vcgencmd) => $cpu_s"
     
     sleep 1
